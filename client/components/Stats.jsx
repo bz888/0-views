919d8baf8c1796e106f97c29230c74b766eeaadc
bz888 <ziyu.ben158@gmail.com>
2022-01-19 19:14:58 +1300

    dcs based searches

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -2,19 +2,10 @@ import React, { useState } from 'react'
 import YouTube from 'react-youtube'
 
 function Stats ({ id, toggle, setToggle, minView }) {
-  const [player, setPlayer] = useState()
-  // const [currentId, setCurrentId] = useState()
-
   function onEnd () {
-    // if (id === currentId) {
     setToggle(!toggle)
-    // }da
   }
 
-  function onReady (event) {
-    setPlayer(event.target)
-    // setCurrentId(id)
-  }
   const opts = {
     height: '390',
     width: '640',
@@ -36,7 +27,6 @@ function Stats ({ id, toggle, setToggle, minView }) {
         videoId={id}
         opts={opts}
         onEnd={onEnd}
-        onReady={onReady}
       />
     </>
   )
